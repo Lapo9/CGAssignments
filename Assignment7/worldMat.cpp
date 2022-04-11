fcce166270097f9c81cfef2e7609e61a4e225d20
Lapo9 <lapofalcone@gmail.com>
2022-04-11 20:54:18 +0200

    Assignment7 done

--- Assignment7/worldMat.cpp
+++ Assignment7/worldMat.cpp
@@ -4,13 +4,17 @@
 // YPR.y : Pitch
 // YPR.z : Roll
 glm::mat4 MakeWorldMatrixEuler(glm::vec3 pos, glm::vec3 YPR, glm::vec3 size) {
-	glm::mat4 out = glm::mat4(1.0f);
-	return out;
+	return
+		glm::translate(glm::mat4(1.0f), pos) *
+		glm::eulerAngleXYX(glm::radians(YPR.x), glm::radians(YPR.y), glm::radians(YPR.z)) *
+		glm::scale(glm::mat4(1.0f), size);
 }
 
 // Create a world matrix using position, quaternion angles, and size
 glm::mat4 MakeWorldMatrixQuat(glm::vec3 pos, glm::quat rQ, glm::vec3 size) {
-	glm::mat4 out = glm::mat4(1.0f);
-	return out;
+	return
+		glm::translate(glm::mat4(1.0f), pos) *
+		glm::mat4(rQ) *
+		glm::scale(glm::mat4(1.0f), size);
 }
 
