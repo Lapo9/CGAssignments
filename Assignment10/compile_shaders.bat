81fb058f685104ec431465f5b54eab6025a028b9
Lapo9 <lapofalcone@gmail.com>
2022-04-23 16:18:29 +0200

    Assignment10 done

---
+++ Assignment10/compile_shaders.bat
@@ -0,0 +1,4 @@
+P:\SDKs\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\PBRShader.vert -o shaders\PBRVert.spv
+P:\SDKs\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\PBRShader.frag -o shaders\PBRFrag.spv
+P:\SDKs\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\SkyBoxShader.vert -o shaders\SkyBoxVert.spv
+P:\SDKs\VulkanSDK\1.3.204.1\Bin\glslc.exe shaders\SkyBoxShader.frag -o shaders\SkyBoxFrag.spv
